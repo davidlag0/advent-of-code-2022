def20a2dd8fb36da1a74fec424501efc5822d6d1
David Laganiere <40720561+davidlag0@users.noreply.github.com>
2022-12-04 14:40:19 -0500

    feat(main): major improvements for cli
    Remove a lot of duplicated code and make it possible to only run a
    specific day and part.

--- src/main.rs
+++ src/main.rs
@@ -1,91 +1,71 @@
-use aoc_rust_2022::day1;
-use aoc_rust_2022::day2;
-use std::env;
+use clap::Parser;
 use std::fs;
-use std::process;
+use std::path::PathBuf;
 use std::time::Instant;
 
-fn load_file(filename: &str) -> std::string::String {
+mod day1;
+mod day2;
+
+#[derive(Parser)]
+#[command(name = "advent-of-code-2022")]
+#[command(about = "Calculate solutions for Advent of Code 2022 using Rust and the provided input", long_about = None)]
+struct Cli {
+    input_path: String,
+    #[arg(short, long)]
+    // TODO: Make this parameter optional and if not provided, solve all days.
+    // day: Option<u8>
+    day: u8,
+    #[arg(short, long)]
+    // TODO: Make this parameter optional and if not provided, solve all parts of a given day.
+    // part: Option<u8>
+    part: u8,
+}
+
+fn load_file(filename: PathBuf) -> std::string::String {
     fs::read_to_string(filename).unwrap()
 }
 
+fn solve(day: u8, part: u8) -> Result<fn(&str) -> Result<i64, &'static str>, (u8, u8)> {
+    match (day, part) {
+        (1, 1) => Ok(day1::part1),
+        (1, 2) => Ok(day1::part2),
+        (2, 1) => Ok(day2::part1),
+        (2, 2) => Ok(day2::part2),
+        (_, _) => Err((day, part)),
+    }
+}
+
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let mut chrono_start;
-    let mut chrono_stop;
-    let mut solution_result;
+    let cli = Cli::parse();
+
+    let chrono_start;
+    let chrono_stop;
+    let solution_result;
     let mut total_time: u128 = 0;
 
-    if args.len() < 2 {
-        println!("not enough arguments");
-        process::exit(1);
+    let day_input: PathBuf = [cli.input_path, format!("day{}.txt", cli.day)]
+        .iter()
+        .collect();
+
+    match solve(cli.day, cli.part) {
+        Ok(solve_function) => {
+            chrono_start = Instant::now();
+            solution_result = solve_function(&load_file(day_input));
+            chrono_stop = chrono_start.elapsed().as_micros();
+            total_time += chrono_stop;
+
+            match solution_result {
+                Ok(solution) => println!(
+                    "Solution of Day {}, Part {}: {:?}, Time: {}μs",
+                    cli.day, cli.part, solution, chrono_stop
+                ),
+                Err(error) => println!(
+                    "A problem occured to solve the problem of Day {}, Part {}: {}, Time: {}μs",
+                    cli.day, cli.part, error, chrono_stop
+                ),
+            }
+            println!("\nTotal Time: {}μs", total_time);
+        }
+        Err(_) => println!("Unsupported day {} and part {}", cli.day, cli.part),
     }
-
-    let base_path = args[1].clone();
-
-    chrono_start = Instant::now();
-    solution_result = day1::part1(&load_file(&[&base_path, "day1.txt"].concat()));
-    chrono_stop = chrono_start.elapsed().as_micros();
-    total_time += chrono_stop;
-
-    match solution_result {
-        Ok(solution) => println!(
-            "Solution of Day 1, Part 1: {:?}, Time: {}μs",
-            solution, chrono_stop
-        ),
-        Err(error) => println!(
-            "A problem occured to solve the problem of Day 1, Part 1: {}, Time: {}μs",
-            error, chrono_stop
-        ),
-    }
-
-    chrono_start = Instant::now();
-    solution_result = day1::part2(&load_file(&[&base_path, "day1.txt"].concat()));
-    chrono_stop = chrono_start.elapsed().as_micros();
-    total_time += chrono_stop;
-
-    match solution_result {
-        Ok(solution) => println!(
-            "Solution of Day 1, Part 2: {:?}, Time: {}μs",
-            solution, chrono_stop
-        ),
-        Err(error) => println!(
-            "A problem occured to solve the problem of Day 1, Part 2: {}, Time: {}μs",
-            error, chrono_stop
-        ),
-    }
-
-    chrono_start = Instant::now();
-    solution_result = day2::part1(&load_file(&[&base_path, "day2.txt"].concat()));
-    chrono_stop = chrono_start.elapsed().as_micros();
-    total_time += chrono_stop;
-
-    match solution_result {
-        Ok(solution) => println!(
-            "Solution of Day 2, Part 1: {:?}, Time: {}μs",
-            solution, chrono_stop
-        ),
-        Err(error) => println!(
-            "A problem occured to solve the problem of Day 2, Part 1: {}, Time: {}μs",
-            error, chrono_stop
-        ),
-    }
-
-    chrono_start = Instant::now();
-    solution_result = day2::part2(&load_file(&[&base_path, "day2.txt"].concat()));
-    chrono_stop = chrono_start.elapsed().as_micros();
-    total_time += chrono_stop;
-
-    match solution_result {
-        Ok(solution) => println!(
-            "Solution of Day 2, Part 2: {:?}, Time: {}μs",
-            solution, chrono_stop
-        ),
-        Err(error) => println!(
-            "A problem occured to solve the problem of Day 2, Part 2: {}, Time: {}μs",
-            error, chrono_stop
-        ),
-    }
-
-    println!("\nTotal Time: {}μs", total_time);
 }
